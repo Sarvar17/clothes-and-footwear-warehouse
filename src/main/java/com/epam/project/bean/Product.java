856a91c4429e2b057944bc2f8dc72dd8d235324b
Sarvar17 <sshilyasov@edu.hse.ru>
2023-05-23 15:55:15 +0500

    Some changes in Product class

--- src/main/java/com/epam/project/bean/Product.java
+++ src/main/java/com/epam/project/bean/Product.java
@@ -11,14 +11,25 @@ public abstract class Product {
     protected String name;
     protected Long id;
 
+    protected int quantity;
+    protected int price;
+
+    public int getQuantity() {
+        return quantity;
+    }
+
     /**
      * Class constructor to set attributes about the product.
      * @param name Name of the product.
      * @param id ID of the product.
+     * @param quantity Quantity of the clothes.
+     * @param price Price of the clothes.
      */
-    public Product(String name, Long id) {
+    public Product(String name, Long id, int quantity, int price) {
         this.name = name;
         this.id = id;
+        this.quantity = quantity;
+        this.price = price;
     }
 
     public String getName() {
@@ -37,17 +48,29 @@ public abstract class Product {
         this.id = id;
     }
 
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Product product = (Product) o;
-        return  Objects.equals(name, product.name) && id.equals(product.id);
+        return quantity == product.quantity && price == product.price && Objects.equals(name, product.name) && id.equals(product.id);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(name, id);
+        return Objects.hash(name, id, quantity, price);
     }
 
     @Override
@@ -55,6 +78,8 @@ public abstract class Product {
         return "Product{" +
                 "name='" + name + '\'' +
                 ", id=" + id +
+                ", quantity=" + quantity +
+                ", price=" + price +
                 '}';
     }
 }
