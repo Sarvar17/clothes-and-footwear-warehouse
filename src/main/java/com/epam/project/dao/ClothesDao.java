d14866032dc3ef02835c4b82af341944ba112173
Sarvar17 <sshilyasov@edu.hse.ru>
2023-05-25 01:09:28 +0500

    Added DAO of Clothes class and Footwear class

---
+++ src/main/java/com/epam/project/dao/ClothesDao.java
@@ -0,0 +1,6 @@
+package com.epam.project.dao;
+
+import com.epam.project.bean.Clothes;
+
+public interface ClothesDao extends ProductDao<Clothes> {
+}
---
+++ src/main/java/com/epam/project/dao/FootwearDao.java
@@ -0,0 +1,6 @@
+package com.epam.project.dao;
+
+import com.epam.project.bean.Footwear;
+
+public interface FootwearDao extends ProductDao<Footwear>{
+}
