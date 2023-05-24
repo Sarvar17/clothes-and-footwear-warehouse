fc97cf46867b6928bca525453c0467ce476ed1c0
Sarvar17 <sshilyasov@edu.hse.ru>
2023-05-25 01:05:49 +0500

    Added color and size parameters to Footwear class

--- src/main/java/com/epam/project/bean/Footwear.java
+++ src/main/java/com/epam/project/bean/Footwear.java
@@ -7,24 +7,39 @@ import java.util.Objects;
  *
  * @author Sarvar Ilyasov
  */
-public class Footwear extends  Product {
-    protected final String category;
+public class Footwear extends  Product implements Comparable<Footwear> {
+    private final String category;
+    private final String color;
+    private final int size;
 
     /**
      * Class constructor to set attributes about the footwear.
      * @param name Name of the footwear.
      * @param id ID of the footwear.
      * @param category Category of the footwear.
+     * @param color Color of the footwear.
+     * @param size Size of the footwear.
      * @param quantity Quantity of the footwear.
      * @param price Price of the footwear.
      */
-    public Footwear(String name, Long id, String category, int quantity, int price) {
+    public Footwear(String name, Long id, String category,
+                    String color, int size, int quantity, int price) {
         super(name, id, quantity, price);
         this.category = category;
+        this.color = color;
+        this.size = size;
     }
 
     public String getCategory() {
         return category;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getSize() {
+        return size;
     }
 
     @Override
@@ -33,22 +48,29 @@ public class Footwear extends  Product {
         if (o == null || getClass() != o.getClass()) return false;
         if (!super.equals(o)) return false;
         Footwear footwear = (Footwear) o;
-        return Objects.equals(category, footwear.category);
+        return size == footwear.size && Objects.equals(category, footwear.category) && Objects.equals(color, footwear.color);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(super.hashCode(), category);
+        return Objects.hash(super.hashCode(), category, color, size);
     }
 
     @Override
     public String toString() {
         return "Footwear{" +
                 "category='" + category + '\'' +
-                ", quantity=" + quantity +
-                ", price=" + price +
+                ", color='" + color + '\'' +
+                ", size=" + size +
                 ", name='" + name + '\'' +
                 ", id=" + id +
+                ", quantity=" + quantity +
+                ", price=" + price +
                 '}';
+    }
+
+    @Override
+    public int compareTo(Footwear o) {
+        return this.getName().compareTo(o.getName());
     }
 }
