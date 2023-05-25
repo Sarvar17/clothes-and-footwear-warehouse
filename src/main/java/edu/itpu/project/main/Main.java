02bc55b984a7390daa011111b704f438770ff2cc
Sarvar17 <sshilyasov@edu.hse.ru>
2023-05-26 01:47:15 +0500

    Added new package main

--- src/main/java/edu/itpu/project/main/Main.java
+++ src/main/java/edu/itpu/project/main/Main.java
@@ -28,35 +28,24 @@ public class Main {
         // Display developer information
         applicationView.displayDeveloperInformation("Sarvar Ilyasov", "sarvar_ilyasov@student.itpu.uz");
 
-        // Display menu
-        applicationView.displayMenu();
-
         // Read user input
         Scanner scanner = new Scanner(System.in);
         boolean exit = false;
 
         while (!exit) {
+            // Display menu
+            applicationView.displayMenu();
+            
             System.out.print("Enter your choice: ");
             int choice = scanner.nextInt();
 
             switch (choice) {
-                case 1:
-                    displayProductsById(scanner, concreteController);
-                    break;
-                case 2:
-                    displayProductsByName(scanner, concreteController);
-                    break;
-                case 3:
-                    displaySortedProducts(concreteController);
-                    break;
-                case 4:
-                    displayPurchasableProducts(scanner, concreteController);
-                    break;
-                case 0:
-                    exit = true;
-                    break;
-                default:
-                    System.out.println("Invalid choice! Please try again.");
+                case 1 -> displayProductsById(scanner, concreteController);
+                case 2 -> displayProductsByName(scanner, concreteController);
+                case 3 -> displaySortedProducts(concreteController);
+                case 4 -> displayPurchasableProducts(scanner, concreteController);
+                case 0 -> exit = true;
+                default -> System.out.println("Invalid choice! Please try again.");
             }
 
             System.out.println();
