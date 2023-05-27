321c56c0ce19cc624ee04370af76312fc6e8f873
Sarvar17 <sshilyasov@edu.hse.ru>
2023-05-27 16:20:38 +0500

    Added tests for service layer

---
+++ src/test/java/edu/itpu/project/service/ClothesServiceImplTest.java
@@ -0,0 +1,112 @@
+package edu.itpu.project.service;
+
+import edu.itpu.project.bean.Clothes;
+import edu.itpu.project.dao.ClothesDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+class ClothesServiceImplTest {
+
+    private ClothesServiceImpl clothesService;
+    private ClothesDao clothesDao;
+
+    @BeforeEach
+    void setUp() {
+        clothesDao = Mockito.mock(ClothesDao.class);
+        clothesService = new ClothesServiceImpl(clothesDao);
+    }
+
+    @Test
+    void getProductsById() {
+        // Mock the data
+        List<Clothes> mockClothesList = Arrays.asList(
+                new Clothes("Ocean Blue Shirt", 1234L, "Shirt",
+                        "Ocean blue", "L", 2, 30),
+                new Clothes("Yellow Wool Jumper", 2345L, "Jumper",
+                        "Yellow", "M", 1, 60)
+        );
+        when(clothesDao.all()).thenReturn(mockClothesList);
+
+        // Call the method under test
+        List<Clothes> result = clothesService.getProductsById(1234L);
+
+        // Verify the result
+        assertEquals(1, result.size());
+        assertEquals("Ocean Blue Shirt", result.get(0).getName());
+        assertEquals(1234L, result.get(0).getId());
+    }
+
+    @Test
+    void getProductsByName() {
+        // Mock the data
+        List<Clothes> mockClothesList = Arrays.asList(
+                new Clothes("Ocean Blue Shirt", 1234L, "Shirt",
+                        "Ocean blue", "L", 2, 30),
+                new Clothes("Ocean Blue Shirt", 5678L, "Shirt",
+                        "Ocean blue", "M", 1, 40)
+        );
+        when(clothesDao.all()).thenReturn(mockClothesList);
+
+        // Call the method under test
+        List<Clothes> result = clothesService.getProductsByName("Ocean Blue Shirt");
+
+        // Verify the result
+        assertEquals(2, result.size());
+        assertEquals("Ocean Blue Shirt", result.get(0).getName());
+        assertEquals("Ocean Blue Shirt", result.get(1).getName());
+    }
+
+    @Test
+    void getSortedProducts() {
+        // Mock the data
+        List<Clothes> mockClothesList = Arrays.asList(
+                new Clothes("Ocean Blue Shirt", 1234L, "Shirt",
+                        "Ocean blue", "L", 2, 30),
+                new Clothes("Yellow Wool Jumper", 2345L, "Jumper",
+                        "Yellow", "M", 1, 60),
+                new Clothes("Black Wool Jumper", 123L, "Jumper",
+                        "Black", "XL", 1, 60)
+        );
+        when(clothesDao.all()).thenReturn(mockClothesList);
+
+        // Call the method under test
+        List<Clothes> result = clothesService.getSortedProducts();
+
+        // Verify the result
+        assertEquals(3, result.size());
+        assertEquals("Black Wool Jumper", result.get(0).getName());
+        assertEquals("Ocean Blue Shirt", result.get(1).getName());
+        assertEquals("Yellow Wool Jumper", result.get(2).getName());
+    }
+
+    @Test
+    void getPurchasableProducts() {
+        // Mock the data
+        List<Clothes> mockClothesList = Arrays.asList(
+                new Clothes("Ocean Blue Shirt", 1234L, "Shirt",
+                        "Ocean blue", "L", 2, 30),
+                new Clothes("Yellow Wool Jumper", 2345L, "Jumper",
+                        "Yellow", "M", 1, 60),
+                new Clothes("Black Wool Jumper", 123L, "Jumper",
+                        "Black", "XL", 1, 60),
+                new Clothes("White Shirt", 234L, "Shirt",
+                        "White", "L", 2, 25)
+        );
+        when(clothesDao.all()).thenReturn(mockClothesList);
+
+        // Call the method under test
+        List<Clothes> result = clothesService.getPurchasableProducts(50);
+
+        // Verify the result
+        assertEquals(2, result.size());
+        assertEquals("Ocean Blue Shirt", result.get(0).getName());
+        assertEquals("White Shirt", result.get(1).getName());
+    }
+}
---
+++ src/test/java/edu/itpu/project/service/FootwearServiceImplTest.java
@@ -0,0 +1,110 @@
+package edu.itpu.project.service;
+
+import edu.itpu.project.bean.Footwear;
+import edu.itpu.project.dao.FootwearDao;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+class FootwearServiceImplTest {
+
+    private FootwearServiceImpl footwearService;
+    private FootwearDao footwearDao;
+
+    @BeforeEach
+    void setUp() {
+        footwearDao = Mockito.mock(FootwearDao.class);
+        footwearService = new FootwearServiceImpl(footwearDao);
+    }
+
+    @Test
+    void getProductsById() {
+        // Mock the data
+        List<Footwear> mockFootwearList = Arrays.asList(
+                new Footwear("Nike Air Jordan 1 Low", 123L, "Sneakers",
+                        "Lucky Green", 40, 2, 110),
+                new Footwear("Adidas Samba Classic", 234L, "Sneakers",
+                        "Black", 42, 1, 90)
+        );
+        when(footwearDao.all()).thenReturn(mockFootwearList);
+
+        // Call the method under test
+        List<Footwear> result = footwearService.getProductsById(123L);
+
+        // Verify the result
+        assertEquals(1, result.size());
+        assertEquals("Nike Air Jordan 1 Low", result.get(0).getName());
+        assertEquals(123L, result.get(0).getId());
+    }
+
+    @Test
+    void getProductsByName() {
+        // Mock the data
+        List<Footwear> mockFootwearList = Arrays.asList(
+                new Footwear("Nike Air Jordan 1 Low", 123L, "Sneakers",
+                        "Lucky Green", 40, 2, 110),
+                new Footwear("Nike Air Jordan 1 Low", 567L, "Sneakers",
+                        "Lucky Green", 41, 1, 120)
+        );
+        when(footwearDao.all()).thenReturn(mockFootwearList);
+
+        // Call the method under test
+        List<Footwear> result = footwearService.getProductsByName("Nike Air Jordan 1 Low");
+
+        // Verify the result
+        assertEquals(2, result.size());
+        assertEquals("Nike Air Jordan 1 Low", result.get(0).getName());
+        assertEquals("Nike Air Jordan 1 Low", result.get(1).getName());
+    }
+
+    @Test
+    void getSortedProducts() {
+        // Mock the data
+        List<Footwear> mockFootwearList = Arrays.asList(
+                new Footwear("Nike Air Jordan 1 Low", 123L, "Sneakers",
+                        "Lucky Green", 40, 2, 110),
+                new Footwear("Adidas Samba Classic", 234L, "Sneakers",
+                        "Black", 42, 1, 90),
+                new Footwear("Adidas Samba Classic", 1234L, "Sneakers",
+                        "White", 40, 1, 90)
+        );
+        when(footwearDao.all()).thenReturn(mockFootwearList);
+
+        // Call the method under test
+        List<Footwear> result = footwearService.getSortedProducts();
+
+        // Verify the result
+        assertEquals(3, result.size());
+        assertEquals("Adidas Samba Classic", result.get(0).getName());
+        assertEquals("Adidas Samba Classic", result.get(1).getName());
+        assertEquals("Nike Air Jordan 1 Low", result.get(2).getName());
+    }
+
+    @Test
+    void getPurchasableProducts() {
+        // Mock the data
+        List<Footwear> mockFootwearList = Arrays.asList(
+                new Footwear("Nike Air Jordan 1 Low", 123L, "Sneakers",
+                        "Lucky Green", 40, 2, 110),
+                new Footwear("Adidas Samba Classic", 234L, "Sneakers",
+                        "Black", 42, 1, 90),
+                new Footwear("Adidas Samba Classic", 1234L, "Sneakers",
+                        "White", 40, 1, 90)
+        );
+        when(footwearDao.all()).thenReturn(mockFootwearList);
+
+        // Call the method under test
+        List<Footwear> result = footwearService.getPurchasableProducts(100);
+
+        // Verify the result
+        assertEquals(2, result.size());
+        assertEquals("Adidas Samba Classic", result.get(0).getName());
+        assertEquals("Adidas Samba Classic", result.get(1).getName());
+    }
+}
